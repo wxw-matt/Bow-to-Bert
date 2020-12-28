719da9d9f043b40edc50e21a9c2f5075be716bc6
Matt <matt.wxw.adelaide@gmail.com>
2020-12-28 17:42:21 +1030

    fix env vars and add shell scripts

---
+++ bert_similarity.sh
@@ -0,0 +1,4 @@
+cd /content/drive/MyDrive/colab/bert/repo
+export PRE_TRAINED_HOME=/content/drive/MyDrive/colab/bert
+export PYTHONPATH=/root/.local/share/virtualenvs/content-cQIIIOO2/lib/python3.6/site-packages
+pipenv run python bert_similarity.py
--- getBertWordVectors.sh
+++ getBertWordVectors.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-input_file=./bert_sentences.txt
-output_file=./bertWordVectors.jsonl
+REPO_DIR=/content/drive/MyDrive/colab/bert/repo
+input_file="$REPO_DIR/bert_sentences.txt"
+output_file="$REPO_DIR/bertWordVectors.jsonl"
+PRE_TRAINED_HOME="/content/drive/MyDrive/colab/bert"
+BERT_BASE_DIR="$PRE_TRAINED_HOME/uncased_L-12_H-768_A-12"
+bert_master=$REPO_DIR/bert_master
 
-BERT_BASE_DIR="$PRE_TRAINED_HOME/bert/uncased_L-12_H-768_A-12"
-bert_master=./bert_master
-
-pipenv run python $bert_master/extract_features.py \
+cd /content/drive/MyDrive/colab/bert/repo/bert_master
+pipenv run python extract_features.py \
   --input_file=$input_file \
   --output_file=$output_file \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
